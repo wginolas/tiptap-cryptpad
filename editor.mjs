45aba5dab372c875f4875307e72f6e4ca8c6c2d1
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-09-23 10:58:30 +0200

    Export tiptap editor

--- editor.mjs
+++ editor.mjs
@@ -1,12 +1,11 @@
 import { Editor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
 
-window.Tiptap = window.Tiptap | {};
-window.Tiptap.Editor = Editor;
-window.Tiptap.StarterKit = StarterKit;
-
-new Editor({
+const editor = new Editor({
   element: document.querySelector('.cp-tiptap-element'),
   extensions: [StarterKit],
   content: '<p>Hello World!</p>',
 })
+
+window.Tiptap = window.Tiptap | {};
+window.Tiptap.editor = editor;
